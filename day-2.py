84f925a18ceb629edbe5240bf8c8fe0815d1fe7b
Kcarry1 <83691169+Kcarry1@users.noreply.github.com>
2021-05-05 12:25:55 +0530

    Add files via upload

---
+++ day-2.py
@@ -0,0 +1,14 @@
+name='harsh'
+salary=2300.34
+age=23
+ismarried=True
+# print(name)
+# # name=int(name)
+# name=bool(name)
+print(type(salary))
+# salary=int(salary)
+# print(salary)
+# salary=str(salary)
+# print(salary)
+salary=bool(salary)
+print(salary)
